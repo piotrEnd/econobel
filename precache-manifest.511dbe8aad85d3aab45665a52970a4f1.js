0b07455443346ed11264d28a0443940271ffef08
piotrEnd <piotrend@gmail.com>
2019-03-20 18:08:44 +0100

    Updates

---
+++ precache-manifest.511dbe8aad85d3aab45665a52970a4f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ea555c342c5e7ab4dd4",
+    "url": "/piotrEnd/econobel/static/js/runtime~main.1543aafa.js"
+  },
+  {
+    "revision": "64c9a60960383ad0ef28",
+    "url": "/piotrEnd/econobel/static/js/main.854896a1.chunk.js"
+  },
+  {
+    "revision": "0173411acfffc5a312ff",
+    "url": "/piotrEnd/econobel/static/js/2.107fe8bb.chunk.js"
+  },
+  {
+    "revision": "64c9a60960383ad0ef28",
+    "url": "/piotrEnd/econobel/static/css/main.e0ac5ea8.chunk.css"
+  },
+  {
+    "revision": "2391fa67d4f0ec898215093c943c990c",
+    "url": "/piotrEnd/econobel/index.html"
+  }
+];
